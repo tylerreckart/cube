614b5f2aa7ad5040b55dd2778c51b4065ff9e1a1
Tyler Reckart <tyler.reckart@gmail.com>
2016-10-18 15:29:07 -0400

    Add listener function to client.js

--- app/client.js
+++ app/client.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 
 import App from './components/index';
 
+document.addEventListener('keypress', (e) => {
+  if (e.keyCode == 32) {
+    e.preventDefault();
+  }
+});
+
 ReactDOM.render((
   <App/>
 ), document.getElementById('root'));
